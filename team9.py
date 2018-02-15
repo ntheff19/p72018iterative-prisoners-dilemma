e61f49f7cb2efddb77acd292780aed16cd4aa30e
ntheff19 <36050232+ntheff19@users.noreply.github.com>
2018-02-15 13:54:57 -0500

    Add files via upload

--- team9.py
+++ team9.py
@@ -6,17 +6,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Tom' # Only 10 chars displayed.
+strategy_name = "Don't betray"
+strategy_description = 'If the oponent has ever betrayed then I will betray but if they have always coluded then I will always colude.'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if 'b' in their_history:
+        return 'b'
+    else:
+        return 'c'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
